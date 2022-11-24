563936ceb15c462eb380e4a610a17f0b5d616f45
TheAniki <ouassim.ouali@live.ca>
2022-11-24 17:11:37 -0500

    Finished calculating score using neighbors

--- TP3/headers/utils.h
+++ TP3/headers/utils.h
@@ -5,5 +5,7 @@ vector<int> split(const string& s, char delim);
 void argumentsParsing(int argc, const char* argv[], string& filename,unsigned int& nbCirconscription, bool& print);
 vector<Municipality> getMunicipalities(ifstream& file, int& x_mun, int& y_mun);
 void displaySolution();
-
+void CalculateScore(vector<Municipality>& municipalities, vector<vector<int>> voteMatrix, int x_mun, int y_mun);
+float scoreFromNeighbors(Coord coord,vector<vector<int>> voteMatrix,int& nbNeighbor, int x_mun, int y_mun);
+bool CoordinateIsValid(int x_coord, int y_coord ,int x_mun, int y_mun);
 #endif // !UTILS_H
