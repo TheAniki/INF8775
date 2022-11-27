989c5e3bc42b1b533d28ac38209760e6d5e1eae7
Philippe <philippe.brunet@polymtl.ca>
2022-11-27 12:50:52 -0500

    compute score

--- TP3/headers/algo.h
+++ TP3/headers/algo.h
@@ -11,10 +11,12 @@ int computeManhattanDist(const Coord& coord1, const Coord& coord2);
 bool validateMunFitsInCirc(shared_ptr<Circumscription> circumscription, shared_ptr<Municipality> municipalityToValidate, int maxDist);
 void addMunicipalityToFirstAvailableCirc(shared_ptr<Municipality> municipality, vector<shared_ptr<Circumscription>> circumscription, bool& isAddedSuccess );
 bool addMunicipalityToFirstAvailableCirc(shared_ptr<Municipality> municipality, 
-        vector<shared_ptr<Circumscription>> circumscriptions, 
+        Solution& solution, 
         int maxDist, 
         int votesToWin,
-        int maxCirc );
+        int maxCirc);
+
+
 vector<shared_ptr<Circumscription>> findIncompleteCircs(vector<shared_ptr<Circumscription>> circumscriptions, int minCirc );
 vector<shared_ptr<Circumscription>> findPossibleCircumscriptionToContain(shared_ptr<Municipality> municipalityToInclude, vector<shared_ptr<Circumscription>> circumscriptionsConsidered, int maxDist);
 #endif // !ALGO_H
